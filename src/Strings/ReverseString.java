ca40f97612d92c57ed72cee4dd652b87cf370684
Sumit Jain <sumi12june@gmail.com>
2015-01-18 20:13:58 -0600

    done

--- src/Strings/ReverseString.java
+++ src/Strings/ReverseString.java
@@ -11,7 +11,7 @@ public class ReverseString {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String a = "sumit jain";
+		String a = "Sumit";
 		ReverseString i = new ReverseString();
 		System.out.println(i.reverse(a));
 
